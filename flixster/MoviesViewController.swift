c76fe389e9b80e918c549888e868756f299a18ed
oreohere <oyendrila.dobe@gmail.com>
2020-09-28 02:08:38 -0400

    Added poster using cocoa pods

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -59,6 +60,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)
+        
+        cell.posterView.af_setImage(withURL: posterURL!) 
         
         return cell
     }
