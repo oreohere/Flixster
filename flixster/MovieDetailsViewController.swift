b33173d87a08f2b46011afd2210ebfbbd2a91809
oreohere <oyendrila.dobe@gmail.com>
2020-10-02 12:46:34 -0400

    Added details on details view

--- flixster/MovieDetailsViewController.swift
+++ flixster/MovieDetailsViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var BackDropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titlelabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
     
     var movie: [String:Any]!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(movie["title"])
+        print(movie["title"] as Any)
         // Do any additional setup after loading the view.
+        titlelabel.text = movie["title"] as? String
+        titlelabel.sizeToFit()
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)
+        
+        posterView.af_setImage(withURL: posterURL!)
+        
+        let baseURLbd = "https://image.tmdb.org/t/p/w780"
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropURL = URL(string: baseURLbd + backdropPath)
+        
+        BackDropView.af_setImage(withURL: backdropURL!)
+        
+        
+        
     }
     
 
