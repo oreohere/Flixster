880593e9a707356a94c396b9acc8af0a17a9bc78
oreohere <oyendrila.dobe@gmail.com>
2020-10-02 15:15:31 -0400

    Added layout as in video

--- flixster/MovieGridViewController.swift
+++ flixster/MovieGridViewController.swift
@@ -19,8 +19,16 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         
         collectionView.delegate = self
         collectionView.dataSource = self
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        layout.minimumLineSpacing = 40
+        layout.minimumInteritemSpacing = 40
+        let width = (view.frame.size.width - layout.minimumInteritemSpacing) / 2
+        layout.itemSize = CGSize(width: width, height: width * 3 / 2)
 
-        let url = URL(string: "https://api.themoviedb.org/3/movie/297762/recommendations?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let url = URL(string: "https://api.themoviedb.org/3/movie/315635/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
